f7b30814c112457382e061e7d049527deaf586bb
balajisunder <balaji.sunder@gmail.com>
2021-02-10 20:17:18 -0600

    c4-complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# setup DB
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # module declaration at bottom to avoid circular dependency
-from app import routes
+from app import routes, models
