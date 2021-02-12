fbe1eaaa649c237f395810a455da322a2aeced8f
balajisunder <balaji.sunder@gmail.com>
2021-02-11 22:04:21 -0600

    C6 Complete

--- app/models.py
+++ app/models.py
@@ -2,13 +2,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 from app import db, login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
+    # query = None
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
@@ -19,6 +23,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        avatar_url = 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return avatar_url
 
 
 @login.user_loader
